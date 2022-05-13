770d4cb6daba27a4270a2fa1e45cb132371d6d25
ivteplo <iteplov7@gmail.com>
2022-05-13 15:32:38 +0300

    Generate a minified version too

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,9 +3,15 @@ const { task, src, dest, series, ...gulp } = require("gulp")
 const sass = require("sass")
 const gulpSass = require("gulp-sass")(sass)
 
+const minify = require("gulp-clean-css")
+const rename = require("gulp-rename")
+
 const build = () =>
   src("sources/main.scss")
     .pipe(gulpSass({ onError: console.error }))
+    .pipe(dest("build/"))
+    .pipe(minify())
+    .pipe(rename({ suffix: ".min" }))
     .pipe(dest("build/"))
 
 const rebuildOnChange = () => gulp.watch("sources/**/*", build)
